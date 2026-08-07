47f816b727b6ca5830e2a12eade5a69e8178405c
Brady <mr.bradytan@gmail.com>
2026-08-06 22:55:36 -0600

    full CRUD on Postgres

--- Backend/CRUD.py
+++ Backend/CRUD.py
@@ -96,12 +96,11 @@ async def create_task(task: Task):
         return JSONResponse(status_code=400, content={"error": "Title is required"})
     if str(task.title).strip("{}") == "":
         return JSONResponse(status_code=400, content={"error": "Task cannot be empty"})
-    with sqlite3.connect("tasks.db") as connection:   
-        cursor = connection.cursor()
-        cursor.execute("INSERT INTO tasks (title, done) VALUES (?, ?);", (task.title, False))
-        connection.commit()
-        cursor.execute("SELECT * FROM tasks WHERE id = ?;", (cursor.lastrowid,))
-        return cursor.fetchone()
+    with psycopg.connect(DATABASE_URL) as conn:
+        with conn.cursor() as cur:
+            cur.execute("INSERT INTO tasks (title, done) VALUES (%s, %s) RETURNING *;", (task.title, False))
+            conn.commit()
+            return cur.fetchone()
 
             
 
@@ -109,61 +108,61 @@ async def create_task(task: Task):
 async def update_task(task: Task):
     if task.title == None or task.done == None or task.id == None or task.created_at == None or task.updated_at == None:
         return JSONResponse(status_code=400, content={"error": "Invalid body"})
-    with sqlite3.connect("tasks.db") as connection:
-        cursor = connection.cursor()
-        try:
-            cursor.execute("BEGIN;")
-            cursor.execute("UPDATE tasks SET title = ?, done = ?, created_at = ?, updated_at = ? WHERE id = ?;", (task.title, task.done, task.created_at, task.updated_at , task.id))
-            connection.commit()
-            if cursor.rowcount > 0 and cursor.rowcount is not None:
-                cursor.execute("SELECT * FROM tasks WHERE id = ?;", (task.id,))
-                return cursor.fetchone()
-            else:
-                return JSONResponse(status_code=404, content={"error": f"Task {task.id} not found"}) 
-                
-        except Exception as e:
-            connection.rollback()
-            return JSONResponse(status_code=500, content={"error": f"Unable to update task: {e}"}) 
+    with psycopg.connect(DATABASE_URL) as conn:
+        with conn.cursor() as cur:
+            try:
+                cur.execute("BEGIN;")
+                cur.execute("UPDATE tasks SET title = %s, done = %s, created_at = %s, updated_at = %s WHERE id = %s RETURNING *;", (task.title, task.done, task.created_at, task.updated_at , task.id))
+                conn.commit()
+                if cur.rowcount > 0 and cur.rowcount is not None:
+                    return cur.fetchone()
+                else:
+                    return JSONResponse(status_code=404, content={"error": f"Task {task.id} not found"})   
+            except Exception as e:
+                conn.rollback()
+                return JSONResponse(status_code=500, content={"error": f"Unable to update task: {e}"}) 
 
 @app.delete("/tasks/{id}", status_code=204)
 async def delete_task(id: int):
-    with sqlite3.connect("tasks.db") as connection:
-        cursor = connection.cursor()
-        try:
-            cursor.execute("BEGIN;")
-            cursor.execute("DELETE FROM tasks WHERE id = ?;",(id,))
-            connection.commit()
-            if cursor.rowcount > 0 and cursor.rowcount is not None:
-                return {"message": "No Content"}
-            else:
-                return JSONResponse(status_code=404, content={"error":"Unknown id"})
-        except Exception as e:
-            connection.rollback()
-            return JSONResponse(status_code=500, content={"error": f"Unable to delete the task: {e}"})
+    with psycopg.connect(DATABASE_URL) as conn:
+        with conn.cursor() as cur:
+            try:
+                cur.execute("BEGIN;")
+                cur.execute("DELETE FROM tasks WHERE id = %s;",(id,))
+                conn.commit()
+                if cur.rowcount > 0 and cur.rowcount is not None:
+                    return {"message": "No Content"}
+                else:
+                    return JSONResponse(status_code=404, content={"error":"Unknown id"})
+            except Exception as e:
+                conn.rollback()
+                return JSONResponse(status_code=500, content={"error": f"Unable to delete the task: {e}"})
     
 
 @app.get("/stats")
 async def get_stats():
-    with sqlite3.connect("tasks.db") as connection:
-        cursor = connection.cursor()
-        total_tasks = cursor.execute("SELECT COUNT(*) FROM tasks;").fetchone()[0]
-        incomplete_tasks = cursor.execute("SELECT COUNT(*) FROM tasks WHERE done = 0;").fetchone()[0]
-        completed_tasks = total_tasks - incomplete_tasks
-        return { "total": total_tasks, "done": completed_tasks, "open": incomplete_tasks}
+    with psycopg.connect(DATABASE_URL) as conn:
+        with conn.cursor() as cur:
+            total_tasks = cur.execute("SELECT COUNT(*) FROM tasks;").fetchone()[0]
+            incomplete_tasks = cur.execute("SELECT COUNT(*) FROM tasks WHERE done = False;").fetchone()[0]
+            completed_tasks = total_tasks - incomplete_tasks
+            return { "total": total_tasks, "done": completed_tasks, "open": incomplete_tasks}
 @app.post("/reset", status_code=204)
 async def reset_tasks():
-   with sqlite3.connect("tasks.db") as connection:
-        cursor = connection.cursor()
-        cursor.execute("DROP TABLE tasks")
-        connection.commit()
-        cursor.execute("""
-        CREATE TABLE IF NOT EXISTS tasks (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            title text NOT NULL,
-            done BOOLEAN NOT NULL CHECK (done IN (0,1)));
-        """)
-        if not cursor.execute("SELECT 1 FROM tasks LIMIT 1").fetchone():
-            cursor.executemany("INSERT INTO tasks (title, done) VALUES (?, ?);", data)
-            connection.commit()
+    with psycopg.connect(DATABASE_URL) as conn:
+        with conn.cursor() as cur:
+            cur.execute("DROP TABLE tasks")
+            conn.commit()
+            cur.execute("""
+            CREATE TABLE IF NOT EXISTS tasks (
+                id serial PRIMARY KEY,
+                title text NOT NULL,
+                created_at text default CURRENT_TIMESTAMP,
+                updated_at text default CURRENT_TIMESTAMP,
+                done boolean NOT NULL);
+            """)
+            if not cur.execute("SELECT 1 FROM tasks LIMIT 1").fetchone():
+                cur.executemany("INSERT INTO tasks (title, done) VALUES (%s, %s);", data)
+                conn.commit()
 
         
